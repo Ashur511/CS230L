80c2ccf4a1a9e6f0de0fc613a348732827690abe
Ashur511 <aej00009@mix.wvu.edu>
2024-02-25 17:16:55 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+<h1>CS 230L</h1>
+<h2>Section - 01</h2>
+<p>WVU ID: 800379786</p>
+<p>Hi I am Ashur Joseph</p>
+    </div>
+  );
+}
+
+export default App;
