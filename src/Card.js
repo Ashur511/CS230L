3691ae7c753a69b088b4d0c75d9c04c1ed0651dc
Ashur511 <143005783+Ashur511@users.noreply.github.com>
2024-03-03 18:46:40 -0500

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,26 @@
-
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card text-center bg-primary text-white" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card 01</h5>
+        <p class="card-text">My favorite food is scrambled eggs.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card text-center bg-danger text-white" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card 02</h5>
+        <p class="card-text">My favorite sport is basketball.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card text-center bg-success text-white" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card 03</h5>
+        <p class="card-text">I am a sophmore computer science student.</p>
+      </div>
+    </div>
+  </div>
+</div>
